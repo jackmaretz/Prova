be70823cfd517715952cd6e942d13f8955cb3620
patriziostella <patrizio.stella@gmail.com>
2015-05-22 16:13:06 +0200

    Update formOpinioni.pl

--- public html/formOpinioni.pl
+++ public html/formOpinioni.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 
 use XML::LibXML;
+use CGI;
 
 my $file = 'opinioni.xml';  #ATTENZIONE! PERCORSO DEL FILE QUANDO CARICO SU SERVER UNIPD!
 
@@ -16,3 +17,21 @@ $nuovaOp = "\n<opinione>
               \n<cognome>$input{'cognomeOpinione'}</cognome>\n
               \n<recensione>$input{'testoOpinione'}</recensione>\n
             \n</opinione>\n";
+
+$gemma = $parser->parse_balanced_chunk($nuovaOp);
+
+$padre->appendChild($frammento);
+
+print OUT $doc->toString;
+
+print "Content-type:text/html\r\n\r\n";
+print "<html>";
+print "<head>";
+print "<title>Hello - Second CGI Program</title>";
+print "</head>";
+print "<body>";
+print "<h2>Hello $first_name $last_name - Second CGI Program</h2>";
+print "</body>";
+print "</html>";
+
+#potrebbe mancare la l di uploadOpinioni
