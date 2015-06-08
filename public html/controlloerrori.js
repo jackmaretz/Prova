f84c72daa473458a3a11c2d0d3809433ecb889b5
patriziostella <patrizio.stella@gmail.com>
2015-06-08 09:46:54 +0200

    Update controlloerrori.js

--- public html/controlloerrori.js
+++ public html/controlloerrori.js
@@ -4,14 +4,17 @@ var testo = document.moduloForm.testoOpinione.value;
 
 function controlloNome () {
   var nome = document.moduloForm.nomeOpinione.value; var errore = "";
-  if (nome.length == 0) errore = 'Inserisci un nome, lungo almeno tre caratteri ' ;
-  elseif (nome.length < 3) errore = 'Inserisci un nome, lungo almeno tre caratteri' ;
-  else if(nome.search("^[A-Z 'a-zàèéìòù]+$")==-1) errore = 'Il nome deve contenere soltanto lettere, spazi o apostrofo.';
-  else if(nome.search("^[A-Za-z][a-z 'A-Zàèéìòù]*[a-zA-Zàèéìòù]$")==-1) errore = 'Il nome può contenere soltanto lettere, non caratteri speciali.';
+  if (nome.length == 0) { errore = 'Inserisci un nome, lungo almeno tre caratteri ' ; sommaErrori = arrayErrori.push("errore"); }
+  elseif (nome.length < 3) {errore = 'Inserisci un nome, lungo almeno tre caratteri' ; sommaErrori = arrayErrori.push("errore"); }
+  else if(nome.search("^[A-Z 'a-zàèéìòù]+$")==-1) {errore = 'Il nome deve contenere soltanto lettere, spazi o apostrofo.'; sommaErrori = arrayErrori.push("errore");}
+  else if(nome.search("^[A-Za-z][a-z 'A-Zàèéìòù]*[a-zA-Zàèéìòù]$")==-1) {errore = 'Il nome può contenere soltanto lettere, non caratteri speciali.'; sommaErrori = arrayErrori.push("errore"); }
   return !!!!!!!!!!!!!!!!! ;
 }
 
 function controlloCognome () {
+  var cognome = document.moduloForm.cognomeOpinione.value; var errore = "";
+  if (cognome.length == 0) errore = 'Inserisci un cognome ' ;
+  elseif (cognome.length < 2) errore = 'Inserisci un cognome, lungo almeno due caratteri' ;
   var cognome = document.moduloForm.cognomeOpinione.value; var errore = "";
   if (cognome.length == 0) errore = 'Inserisci un cognome ' ;
   elseif (cognome.length < 2) errore = 'Inserisci un cognome, lungo almeno due caratteri' ;
@@ -24,5 +27,14 @@ function controlloTesto () {
   var testo = document.moduloForm.testoOpinione.value; var errore = "";
   if(testo.lenght == 0) errore = 'Inserire il testo dell'opinione' ;
   return !!!!!!!!!!!!!!!!!! ;
+}
+
+function listaerrori () {
+  arrayErrori = new array();
+  alert(Gli errori commessi nella compilazione del form: 
+  for(i=0; i<arrayErrori.length; i++;) {
+    arrayErrori[i];
+  }
+  
 }
 
