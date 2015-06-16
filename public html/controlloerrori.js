2f857dd04e212bd1d1234346f82895d7158e3a2f
jackmaretz <giacomomaretto@gmail.com>
2015-06-16 14:48:10 +0200

    s

--- public html/controlloerrori.js
+++ public html/controlloerrori.js
@@ -5,8 +5,9 @@ var testo = document.moduloForm.testoOpinione.value;
 function controlloNome () {
   var nome = document.moduloForm.nomeOpinione.value; var errore = "";
   if (nome.length == 0) { errore = 'Inserisci un nome, lungo almeno tre caratteri ' ; sommaErrori = arrayErrori.push("errore"); }
-  elseif (nome.length < 3) {errore = 'Inserisci un nome, lungo almeno tre caratteri' ; sommaErrori = arrayErrori.push("errore"); }
+  else if (nome.length < 3) {errore = 'Inserisci un nome, lungo almeno tre caratteri' ; sommaErrori = arrayErrori.push("errore"); }
   else if(nome.search("^[A-Z 'a-zàèéìòù]+$")==-1) {errore = 'Il nome deve contenere soltanto lettere, spazi o apostrofo.'; sommaErrori = arrayErrori.push("errore");}
+  //ATTEBZIONE GRAFFE
   else if(nome.search("^[A-Za-z][a-z 'A-Zàèéìòù]*[a-zA-Zàèéìòù]$")==-1) {errore = 'Il nome può contenere soltanto lettere, non caratteri speciali.'; sommaErrori = arrayErrori.push("errore"); }
   return !!!!!!!!!!!!!!!!! ;
 }
@@ -14,10 +15,10 @@ function controlloNome () {
 function controlloCognome () {
   var cognome = document.moduloForm.cognomeOpinione.value; var errore = "";
   if (cognome.length == 0) errore = 'Inserisci un cognome ' ;
-  elseif (cognome.length < 2) errore = 'Inserisci un cognome, lungo almeno due caratteri' ;
+  else if (cognome.length < 2) errore = 'Inserisci un cognome, lungo almeno due caratteri' ;
   var cognome = document.moduloForm.cognomeOpinione.value; var errore = "";
   if (cognome.length == 0) errore = 'Inserisci un cognome ' ;
-  elseif (cognome.length < 2) errore = 'Inserisci un cognome, lungo almeno due caratteri' ;
+  else if (cognome.length < 2) errore = 'Inserisci un cognome, lungo almeno due caratteri' ;
   else if(cognome.search("^[A-Z 'a-zàèéìòù]+$")==-1) errore = 'Il cognome deve contenere soltanto lettere, spazi o apostrofo.';
   else if(cognome.search("^[A-Za-z][a-z 'A-Zàèéìòù]*[a-zA-Zàèéìòù]$")==-1) errore = 'Il cognome può contenere soltanto lettere, non caratteri speciali.';
   return !!!!!!!!!!!!!!!!! ;
